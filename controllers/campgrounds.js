10e65c500f136150768058b3e981986e90a8804c
ShereenLiao <xl3781@nyu.edu>
2022-07-27 23:33:59 -0400

    feat:add quer

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -9,41 +9,60 @@ const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 // *********************************************
 // INDEX - renders multiple campgrounds
 // **************************˜*******************
-module.exports.index=async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
+module.exports.index = async (req, res, next) => {
+    // console.log(req.query);
+    const {campground={}}=req.query;
+    if(!campground){
+        const campgrounds = await Campground.find({});
+        return res.render('campgrounds/index', { campgrounds,campground})
+    }
+    var cond={}
+    const {price,title,location}=campground;
+    if(price){
+        cond.price=parseInt(price);
+    }
+    if(title){
+        cond.title=title;
+    }
+    if(location){
+        cond.location=location;
+    }
+    const campgrounds=await Campground.find(cond);
+    return res.render('campgrounds/index', { campgrounds,campground})
 };
 
 // **********************************************
 // NEW - renders a form
 // **********************************************
-module.exports.renderNewForm=(req, res) => {
+module.exports.renderNewForm = (req, res) => {
     res.render('campgrounds/new');
 };
 
 // **********************************************
 // CREATE - creates a new campground
 // **********************************************
-module.exports.createCampground=async (req, res, next) => {
+module.exports.createCampground = async (req, res, next) => {
     const geoData = await geocoder.forwardGeocode({
         query: req.body.campground.location,
         limit: 1
     }).send()
     if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
-    campground.geometry=geoData.body.features[0].geometry;
+    campground.geometry = geoData.body.features[0].geometry;
     campground.author = req.user._id;
-    campground.images=req.files.map(f=>({url:f.path,filename:f.filename}));
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     await campground.save();
     console.log(campground);
     req.flash('success', 'Successfully created a new campground'); //store a flash message if success
     res.redirect(`/campgrounds/${campground._id}`)
 };
 
+
+
 // ***********************************************
 // SHOW - details about one particular campground
 // ***********************************************
-module.exports.showCampground=async (req, res,) => {
+module.exports.showCampground = async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate({
         path: 'reviews',
         populate: {
@@ -61,7 +80,7 @@ module.exports.showCampground=async (req, res,) => {
 // ***********************************************
 // EDIT - renders a form to edit a campground
 // ***********************************************
-module.exports.renderEditForm=async (req, res) => {
+module.exports.renderEditForm = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground) {
@@ -74,9 +93,9 @@ module.exports.renderEditForm=async (req, res) => {
 // ***********************************************
 // UPDATE - updates a particular campground
 // ***********************************************
-module.exports.updateCampground=async (req, res) => {
+module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
-    const campground=await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
     await campground.save();
@@ -93,7 +112,7 @@ module.exports.updateCampground=async (req, res) => {
 // ***********************************************
 // DELETE - delete a particular campground
 // ***********************************************
-module.exports.deleteCampground=async (req, res) => {
+module.exports.deleteCampground = async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted campground')
